b35268b17e570b67c9a88860fa32752270427eef
kysshsy <kysshsy@gmail.com>
2021-07-23 16:52:27 +0800

    feature(context): 增加context接口细节

--- kee/gee.go
+++ kee/gee.go
@@ -2,6 +2,7 @@ package kee
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -24,8 +25,12 @@ func (e *Engine) ServeHTTP(respWriter http.ResponseWriter, req *http.Request) {
 
 }
 
-func (e *Engine) GET(url string, handlerFunc func(*Context)) {
-	e.Router.AddRoute("GET", url, handlerFunc)
+func (e *Engine) GET(url string, handler func(*Context)) {
+	e.Router.AddRoute("GET", url, handler)
+}
+
+func (e *Engine) POST(url string, handler func(*Context)) {
+	e.Router.AddRoute("POST", url, handler)
 }
 
 type H map[string]interface{}
@@ -44,6 +49,28 @@ func NewContext(writer http.ResponseWriter, req *http.Request) *Context {
 	return &Context{Writer: writer, Req: req, Url: req.URL.Path, Method: req.Method}
 }
 
+// getter
+
+func (c *Context) Query(key string) string {
+	values := c.Req.Header.Values(key)
+
+	if len(values) == 0 {
+		return ""
+	}
+
+	return values[0]
+}
+
+func (c *Context) QueryArray(key string) []string {
+	return c.Req.Header.Values(key)
+}
+
+func (c *Context) GetForm(key string) string {
+	return c.Req.Form.Get(key)
+}
+
+// setters
+
 func (c *Context) SetHeader(key string, value string) {
 	c.Writer.Header().Set(key, value)
 }
@@ -57,6 +84,12 @@ func (c *Context) Error(code int, emsg string) {
 	http.Error(c.Writer, emsg, code)
 }
 
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Contnt-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
 func (c *Context) Json(code int, obj interface{}) {
 	c.SetHeader("Content-Type", "application/json")
 
@@ -66,4 +99,15 @@ func (c *Context) Json(code int, obj interface{}) {
 	}
 	c.Status(code)
 	c.Writer.Write(jsonByte)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
 }
