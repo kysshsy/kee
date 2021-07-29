67ffbf89a1492940785c95a2d7676ed1872ffe60
kysshsy <kysshsy@gmail.com>
2021-07-29 18:16:32 +0800

    feature(middleware): 中间件测试

--- main.go
+++ main.go
@@ -24,38 +24,36 @@ $ curl "http://localhost:9999/xxx"
 
 import (
 	"kee"
+	"log"
 	"net/http"
+	"time"
 )
+
+func onlyForV2() kee.HandlerFunc {
+	return func(c *kee.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
 
 func main() {
 	r := kee.New()
-	r.GET("/index", func(c *kee.Context) {
-		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	r.Use(kee.Logger()) // global midlleware
+	r.GET("/", func(c *kee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello kee</h1>")
 	})
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/", func(c *kee.Context) {
-			c.HTML(http.StatusOK, "<h1>Hello kee</h1>")
-		})
 
-		v1.GET("/hello", func(c *kee.Context) {
-			// expect /hello?name=keektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-		})
-	}
 	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
 	{
 		v2.GET("/hello/:name", func(c *kee.Context) {
 			// expect /hello/keektutu
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 		})
-		v2.POST("/login", func(c *kee.Context) {
-			c.JSON(http.StatusOK, kee.H{
-				"username": c.PostForm("username"),
-				"password": c.PostForm("password"),
-			})
-		})
-
 	}
 
 	r.Run(":9999")
